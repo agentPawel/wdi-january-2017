12c9c7706604fc499a433c35b64e4919d3af72ec
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-02 12:14:52 -0500

    Add scopes

--- 19-migrations-and-validations/chinook/chinook_app/app/models/track.rb
+++ 19-migrations-and-validations/chinook/chinook_app/app/models/track.rb
@@ -1,4 +1,15 @@
 class Track < ActiveRecord::Base
+  
+  SHORT = 180000
+  LONG  = 360000
+  
+  scope :short,  -> { shorter_than_or_equal_to(SHORT) }
+  scope :long,   -> { where('milliseconds >= ?', LONG) }
+  scope :medium, -> { where('milliseconds > ? AND milliseconds < ?', SHORT, LONG) }
+  
+  scope :shorter_than_or_equal_to, -> (milliseconds) { where('milliseconds <= ?', milliseconds) }
+  
+  scope :starts_with, -> (first_char) { where('name ILIKE ?', "#{ first_char }%") }
 
   validates :name, presence: true
   
