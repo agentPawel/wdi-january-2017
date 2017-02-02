c862f92232a5cc72f63f2fab564a8425cb1139d9
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-02 11:42:35 -0500

    add custom validation method to Track model

--- 19-migrations-and-validations/chinook/chinook_app/app/models/track.rb
+++ 19-migrations-and-validations/chinook/chinook_app/app/models/track.rb
@@ -1,2 +1,24 @@
 class Track < ActiveRecord::Base
+
+  validates :name, presence: true
+  
+  validate  :name_must_be_titleized
+  
+  validates :milliseconds, presence: true,
+                           numericality: { greater_than: 0 }
+              
+  validates :unit_price, presence: true,
+                         numericality: { greater_than_or_equal_to: 0.0 }
+  
+private
+  
+  def name_must_be_titleized
+    first_char = name[0]
+    first_char_is_not_upcased = (first_char != first_char.upcase)
+    
+    if first_char_is_not_upcased
+      errors.add(:name, 'is not a proper title')
+    end
+  end
+  
 end
