fa97b8504283c42910f815184a8c1171a9db7c72
NatalieBlack <natalievictoriablack@gmail.com>
2017-02-03 12:36:00 -0500

    in-class code for 20-data-modelling-associations

---
+++ 20-data-modelling-associations/photogur/app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :picture #comments table has a picture_id col
+  # @comment.picture
+  # @comment.picture =
+  # etc
+
+end
--- 20-data-modelling-associations/photogur/app/models/picture.rb
+++ 20-data-modelling-associations/photogur/app/models/picture.rb
@@ -1,2 +1,8 @@
 class Picture < ActiveRecord::Base
+  has_many :comments #comments table has a picture_id column
+  # @picture.comments
+  # @picture.comments <<
+  # @picture.comments.where(...)
+  # @picture.comments.count
+  # etc
 end
