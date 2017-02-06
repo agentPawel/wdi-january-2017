9e0c483f0b3c43240cab6043013dedf5f5306917
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-06 11:26:21 -0500

    Implement users#new and the user registration form

--- 21-sessions-and-authentication/authentication/config/routes.rb
+++ 21-sessions-and-authentication/authentication/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   #get 'users/create'
   
   resource :users, only: %i(new create)
+  
+  root 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
