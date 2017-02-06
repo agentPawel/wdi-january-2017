e2e1f73fb2b8c558c2a7a88e420054e3f6df46ff
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-06 12:06:46 -0500

    Implenent login

--- 21-sessions-and-authentication/authentication/app/controllers/sessions_controller.rb
+++ 21-sessions-and-authentication/authentication/app/controllers/sessions_controller.rb
@@ -3,8 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: session_params[:email])
+    
+    if user && user.authenticate(session_params[:password])
+      flash[:notice] = 'Successfully logged in!'
+      redirect_to root_url
+    else
+      flash.now[:alert] = 'Try again :('
+      render :new
+    end
+    
   end
 
   def destroy
   end
+  
+  private
+  
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+  
 end
