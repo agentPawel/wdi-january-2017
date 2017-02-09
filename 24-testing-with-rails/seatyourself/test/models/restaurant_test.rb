6feaa56732426b9bca2cce52839d5279981cdd44
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-09 10:51:43 -0500

    Add simple second restaurant test

--- 24-testing-with-rails/seatyourself/test/models/restaurant_test.rb
+++ 24-testing-with-rails/seatyourself/test/models/restaurant_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
+  
   test 'two restaurants exist' do
     assert_equal 2, Restaurant.all.size
+  end
+  
+  test 'small restaurant has small capacity' do
+    assert restaurants(:small_restaurant).capacity <= 25
   end
   
 end
