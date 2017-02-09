447e6457ed7938399e96aedb18cd8a6802ee8cc9
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-02-09 12:27:07 -0500

    Restaurant#available? now takes time into account

--- 24-testing-with-rails/seatyourself/app/models/restaurant.rb
+++ 24-testing-with-rails/seatyourself/app/models/restaurant.rb
@@ -7,14 +7,19 @@ class Restaurant < ApplicationRecord
   #  
   #end
   
-  def available?(party_size)
-    party_size > 0 && available_capacity >= party_size
+  def available?(party_size, time)
+    party_size > 0 && available_capacity(time) >= party_size
   end
   
   private
   
-  def available_capacity
-    capacity - reservations.sum(:party_size)
+  def available_capacity(time)
+    capacity - reservations_at(time).sum(:party_size)
+  end
+  
+  def reservations_at(time)
+    #reservations.where('time > ? AND time < ?', time.beginning_of_hour, time.end_of_hour)
+    reservations.where(time: time.beginning_of_hour..time.end_of_hour)
   end
   
 end
