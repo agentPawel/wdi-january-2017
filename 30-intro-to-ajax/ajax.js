8ee92b080868f0ef921e5fc6e1a7ad80a9d60cf9
Pawel Parafianowicz <pawelp@outlook.com>
2017-02-16 12:57:15 -0500

    complete

--- 30-intro-to-ajax/ajax.js
+++ 30-intro-to-ajax/ajax.js
@@ -1,3 +1,42 @@
 $(function() {
 
+  $('#load-monsters').on('click', function() {
+    $.ajax({
+      crossDomain: false, // Needed for dataType because monsters-api is not the same origin
+      url: 'http://monsters-api.herokuapp.com/monsters',
+      data: { limit: 5 },
+      dataType: 'html',
+      method: 'GET'
+    }).done(function(data) {
+      $('body').append(data);
+    }).fail(function(jqXHR, textStatus, errorThrown){
+      console.log('Ajax Request Failed!');
+      console.log(jqXHR);
+    }).always(function(){
+      console.log('Ajax Request Sent!');
+    });
+
+    console.log('Next Line after .ajax')
+  });
+});
+
+  $('#add-monster').on('click', function() {
+    $.ajax({
+      // crossDomain: false, // Needed for dataType because monsters-api is not the same origin
+      url: 'http://monsters-api.herokuapp.com/monsters',
+      data: {
+        monster: {
+          name: 'Frankenstein',
+          home: 'Transylvania',
+          creepiness: 15
+        }
+      },
+      method: 'POST'
+    }).done(function(data) {
+      $('body').append('SUCCESS: Monster Created!');
+    }).fail(function(){
+      $('body').append('ERROR: Could not create a monster.');
+    }).always(function(){
+      $('body').append('Add operation complete.');
+    });
 });
